17bdf09552be8935a07bb1c347fea258d4f68b60
sin do <sin.do@mobiclixgroup.com>
2019-07-06 01:29:06 +0700

    Demo Test

--- CICDDemoTests/CICDDemoTests.swift
+++ CICDDemoTests/CICDDemoTests.swift
@@ -19,15 +19,6 @@ class CICDDemoTests: XCTestCase {
     }
 
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertTrue(1 == 1)
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
