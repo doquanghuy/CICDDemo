6102898ee51c66968f139d4f2d6bf834a3c971d1
sin do <sin.do@mobiclixgroup.com>
2019-07-06 14:16:51 +0700

    App Delegate 21

--- CICDDemo/AppDelegate.swift
+++ CICDDemo/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        print(20)
+        print(21)
         return true
     }
 
